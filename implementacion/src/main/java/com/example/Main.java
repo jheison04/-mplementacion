5df191278a39c5ac54b204c41111f31a9b75139c
jheison-stiven-londoño-cruz <jheisonstiven04@gmail.com>
2026-03-11 09:33:52 -0500

    trabajo de implementacion

--- implementacion/src/main/java/com/example/Main.java
+++ implementacion/src/main/java/com/example/Main.java
@@ -1,7 +1,29 @@
 package com.example;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Producto usando constructor por defecto
+        Producto p1 = new Producto();
+
+        p1.setId(1);
+        p1.setNombre("Laptop");
+        p1.setPrecio(1200);
+        p1.setStock(5);
+
+        // Producto usando constructor parametrizado
+        Producto p2 = new Producto(2, "Mouse", 50, 20);
+
+        // Mostrar información
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+
+        // Modificar datos
+        p2.setPrecio(45);
+        p2.setStock(25);
+
+        System.out.println("Producto actualizado:");
+        System.out.println(p2.toString());
     }
 }
